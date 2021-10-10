b1b4857d0186e9e1c4b81e5fc22dded0db544c23
Russell Warlow <52016758+russell-warlow@users.noreply.github.com>
2021-10-10 12:31:29 -0700

    deleted extra code and removed log messages

--- script.js
+++ script.js
@@ -1,70 +1,9 @@
-/* 
-
-make [n]x[n] grid
-div for each row
--> div for each column
-
-*/
-
-function createRow(n) {
-    const newRow = document.createElement('div');
-    newRow.className = 'row';
-    for(let i=0; i<n; i++) {
-        const newDiv = document.createElement('div');
-        newDiv.className = 'square';
-        // const newText = document.createElement('text');
-        // newText.textContent = 'foo';
-        // newDiv.appendChild(newText);
-        newRow.appendChild(newDiv);
-    }
-    const mainBody = document.getElementById('main');
-    mainBody.appendChild(newRow);
-}
-
 function createGrid(n) {
-    for(let i=0; i<n; i++) {
-        createRow(n);
-    }
-}
-
-function createGridNoRowGaps(n) {
-    // calculate height and width of box
     const mainDiv = document.getElementById('main');
-    console.log('maindiv: ' + mainDiv);
-    const width = 960; // why doesn't clientWidth work?!
-    console.log('width: ' + width);
+    const widthInPixels = getComputedStyle(mainDiv).width; // why doesn't clientWidth work?!
+    let width = parseInt(widthInPixels.substring(0, widthInPixels.length-2));
     const gap = 10;
-    console.log('gap: ' + gap);
-    // per row ... [n] boxes, [n-1] gaps?
-    const squareLength = (width-(n-1)*gap)/n;
-    console.log('square dimension: ' + squareLength);
-    for(let i=0;i<n; i++) {
-        let newRow = document.createElement('div');
-        newRow.style.height = squareLength + 'px';
-        newRow.style.width = width + 'px';
-        newRow.className = 'row';
-        for(let j=0; j<n; j++) {
-            let newDiv = document.createElement('div');
-            newDiv.className = 'square';
-            newDiv.style.height = squareLength + 'px';
-            newDiv.style.width = squareLength + 'px';
-            newRow.appendChild(newDiv);
-        }
-        mainDiv.appendChild(newRow);
-    }
-}
-
-function createGridWrap(n) {
-    // calculate height and width of box
-    const mainDiv = document.getElementById('main');
-    console.log('maindiv: ' + mainDiv);
-    const width = 960; // why doesn't clientWidth work?!
-    console.log('width: ' + width);
-    const gap = 10;
-    console.log('gap: ' + gap);
-    // per row ... [n] boxes, [n-1] gaps?
-    const squareLength = (width-(n-1)*gap)/n;
-    console.log('square dimension: ' + squareLength);
+    const squareLength = (width-(n-1)*gap)/n; // per row ... [n] boxes, [n-1] gaps?
     let newRow = document.createElement('div');
     newRow.style.height = squareLength + 'px';
     newRow.style.width = width + 'px';
@@ -94,27 +33,17 @@ function refreshPage(event) {
     let newGridSize = parseInt(userInput);
     if(isNaN(newGridSize)) {
         alert('please enter a number');
-        console.log('please enter a number');
         refreshPage(event);
     }
     if(newGridSize > 100) {
         alert('number too large. please enter number less than or equal to 100');
-        console.log('number too large. please enter number less than or equal to 100');
         refreshPage(event);
     }
-        
     const nodes = Array.from(document.getElementsByClassName('row'));
-    if(nodes.length > 1) {
-        console.log('too many rows.');
-        return;
-    }
-    if(nodes.length == 1) {
-        nodes[0].remove();
-    }
-
-    createGridWrap(newGridSize);
+    nodes[0].remove();
+    createGrid(newGridSize);
 }
 
 const button = document.getElementById('refresh');
 button.addEventListener('click', refreshPage);
-createGridWrap(8);
+createGrid(8);
